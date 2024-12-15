bef36ca127a33702f43769760bcdbc80bf193c84
Azizazf <fayeabdou13@gmail.com>
2024-12-15 13:30:41 +0000

    Checkpoint React typescript

---
+++ src/appp.tsx
@@ -0,0 +1,31 @@
+// Code o2
+
+import React, { Component } from "react";
+
+
+interface CounterState {
+count: number;
+}
+
+class Counter extends Component<{}, CounterState> {
+state: CounterState = {
+    count: 0,
+};
+
+increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+};
+render() {
+    return (
+    <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+    </div>
+    );
+}
+}
+
+export default Counter;
+function render() {
+throw new Error("Function not implemented.");
+}
